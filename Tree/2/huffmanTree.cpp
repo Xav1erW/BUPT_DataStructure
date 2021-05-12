507caf0faca913d04389f7487e42b0910155b4f1
xav1er <xavierwang035@gmail.com>
2021-05-12 09:44:46 +0800

    删除无用注释代码

--- Tree/2/huffmanTree.cpp
+++ Tree/2/huffmanTree.cpp
@@ -65,8 +65,6 @@ huffmanTree::huffmanTree(ifstream& file)
 
 void huffmanTree::generateTable(const node *r, string binCode)
 {
-    // cout << binCode << endl;
-
     if (r != nullptr)
     {
         if (r->left == nullptr && r->right == nullptr)      //左右都为空，为叶子节点，构建表
@@ -83,10 +81,6 @@ void huffmanTree::generateTable(const node *r, string binCode)
     }
 }
 
-// void huffmanTree::generateTable(const node *r, string binCode)
-// {
-
-// }
 
 
 void huffmanTree::showTable()
@@ -118,7 +112,6 @@ void huffmanTree::encode(string address)
     ofstream file(address, ios::binary);
     file.seekp(sizeof(huffmanHead));
     huffmanHead header;
-    // header.codeMapLength = encodeMap.size();
     for (auto it = decodeMap.begin(); it != decodeMap.end(); it++)
     {
         header.tableSize += it->first.length() + 1;
@@ -130,20 +123,6 @@ void huffmanTree::encode(string address)
     for (char ch : text)
     {
         code += encodeMap[ch];
-        // if (code.length() + encodeMap[ch].length() <= BUFFERSIZE)
-        // {
-        //     code += encodeMap[ch];
-        // }
-        // else
-        // {
-        //     //如果buffer存满了，开始写入
-        //     int len = BUFFERSIZE - code.length();
-        //     code += encodeMap[ch].substr(0, len);
-
-            // code = "";
-            // code += encodeMap[ch].substr(len);
-            // cout << "buffer full" << endl;
-        // }
     }
     cout << "code:" << code << endl;
     //按字节写入文件
@@ -172,37 +151,6 @@ void huffmanTree::encode(string address)
     //不足一个字节的bit数
     header.remainedBits = code.length() % 8;
 
-    // //保存最后未存满一个buffer的数据
-    // int counter = 0;
-    // for (auto it = code.begin(); it != code.end() - header.remainedBits; it++)
-    // {
-    //     //按字节写入文件
-    //     if (counter%8 == 0 && counter!= 0)
-    //     {
-    //         file.write((char *)&byteBuffer, 1);
-    //         byteBuffer = 0;
-    //         byteBuffer << 1;
-    //         byteBuffer += *it - '0';
-    //         counter++;
-    //         header.dataSize ++;
-    //     }
-    //     else
-    //     {
-    //         byteBuffer << 1;
-    //         byteBuffer += *it - '0';
-    //         counter++;
-    //     }
-    // }
-    // //写入剩余的buffer内容
-    // file.write((char *)&byteBuffer, 1);
-    // header.dataSize++;
-
-    // byteBuffer = 0;
-    // for (auto it = code.end() - header.remainedBits; it != code.end(); it++)
-    // {
-    //     byteBuffer << 1;
-    //     byteBuffer += *it;
-    // }
     //写入不足八位的编码
     file.write((char *)&byteBuffer, 1);
     header.dataSize++;
@@ -264,21 +212,12 @@ void decode(string in, string out)
         }
     }
 
-    // auto dataStart = fileIn.tellg();
-    // fileIn.seekg(0, fileIn.end);
-    // int dataSize = fileIn.tellg()-dataStart;      //数据字节数
-    // int outOfBuffer = dataSize % BUFFERSIZE;
-    // fileIn.seekg(dataStart);        //回到数据开始的地方
-
     char* dataIn = new char[header.dataSize];
     string outputText;
     unsigned char mask = 128;           //二进制10000000，用于利用and取最高位
     code = "";
-    // int dataLeft = header.dataSize;               //还未读入的字节数
-    // while(dataLeft != 0)
     
     fileIn.read(dataIn, header.dataSize);
-    // dataLeft = 0;
     int i = 0;
     int bitCount = 0;               //记录已经读取的bit数，每8位读取下一字节
     unsigned char byte = 0;
@@ -327,97 +266,6 @@ void decode(string in, string out)
             code = "";              //code置零，读取下一个编码准备
         }
     }
-    
-        // if(dataSize == outOfBuffer)
-        // {
-        //     fileIn.read(bufferIn, outOfBuffer);
-        //     dataSize = 0;
-        //     int i = 0;
-        //     int bitCount = 0;               //记录已经读取的bit数，每8位读取下一字节
-        //     unsigned char byte = 0;
-        //     while (i != outOfBuffer-1)             //逐个读取buffer中的数据
-        //     {
-        //         if(bitCount % 8 == 0)
-        //         {
-        //             byte = bufferIn[i];
-        //             i++;
-        //             char bitByChar = byte&&mask + '0';   //用字符‘0’‘1’存储单个bit
-        //             byte << 1;
-        //             bitCount ++;
-        //             code.push_back(bitByChar);
-        //             if(decodeMap.find(code) != decodeMap.end())      //在hash表中找到存在的对应字符
-        //             {
-        //                 outputText.push_back(decodeMap[code]);
-        //                 code = "";              //code置零，读取下一个编码准备
-        //             }
-        //         }
-        //         else
-        //         {
-        //             char bitByChar = byte&&mask + '0';   //用字符‘0’‘1’存储单个bit
-        //             byte << 1;
-        //             bitCount ++;
-        //             code.push_back(bitByChar);
-        //             if(decodeMap.find(code) != decodeMap.end())     //在hash表中找到存在的对应字符
-        //             {
-        //                 outputText.push_back(decodeMap[code]);
-        //                 code = "";          //code置零，读取下一个编码准备
-        //             }
-        //         }
-        //     }
-        //     //读取不足一字节的数据
-        //     byte = bufferIn[outOfBuffer-1];
-        //     byte << 8-header.remainedBits;
-        //     bitCount = 8-header.remainedBits;
-        //     while(bitCount != 8)
-        //     {
-        //         char bitByChar = byte&&mask+'0';
-        //         byte << 1;
-        //         bitCount++;
-        //         code.push_back(bitByChar);
-        //         if(decodeMap.find(code) != decodeMap.end())     //在hash表中找到存在的对应字符
-        //         {
-        //             outputText.push_back(decodeMap[code]);
-        //             code = "";              //code置零，读取下一个编码准备
-        //         }
-        //     }
-        // }
-        // else
-        // {
-        //     fileIn.read(bufferIn, BUFFERSIZE);
-        //     dataSize -= BUFFERSIZE;
-        //     int i = 0;
-        //     int bitCount = 0;               //记录已经读取的bit数，每8位读取下一字节
-        //     unsigned char byte = 0;
-        //     while (i != BUFFERSIZE)             //逐个读取buffer中的数据
-        //     {
-        //         if(bitCount % 8 == 0)
-        //         {
-        //             byte = bufferIn[i];
-        //             i++;
-        //             char bitByChar = byte&&mask + '0';   //用字符‘0’‘1’存储单个bit
-        //             byte << 1;
-        //             bitCount ++;
-        //             code.push_back(bitByChar);
-        //             if(decodeMap.find(code) != decodeMap.end())      //在hash表中找到存在的对应字符
-        //             {
-        //                 outputText.push_back(decodeMap[code]);
-        //                 code = "";              //code置零，读取下一个编码准备
-        //             }
-        //         }
-        //         else
-        //         {
-        //             char bitByChar = byte&&mask + '0';   //用字符‘0’‘1’存储单个bit
-        //             byte << 1;
-        //             bitCount ++;
-        //             code.push_back(bitByChar);
-        //             if(decodeMap.find(code) != decodeMap.end())     //在hash表中找到存在的对应字符
-        //             {
-        //                 outputText.push_back(decodeMap[code]);
-        //                 code = "";              //code置零，读取下一个编码准备
-        //             }
-        //         }
-        //     }
-        // }
     
     delete[] dataIn;
     cout << outputText << endl;
